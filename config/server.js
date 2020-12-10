217d1c87389cc51b7c9987f492c5395ab057de2f
Felipe Marinho <felipmarinho@gmail.com>
2020-12-10 07:03:37 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0ba7110cc668b61cbbc7bf391914b4f0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9fcca3fb-b579-4189-8f85-4685d9268a9d'
+};
