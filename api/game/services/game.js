1284c9f2a2652db197d0c24ab305df308443b25c
Felipe Marinho <felipmarinho@gmail.com>
2020-12-25 18:40:03 -0300

    fetch data of the api gog

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -4,11 +4,12 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
+const axios = require('axios')
 
 module.exports = {
   populate: async (params) => {
-    const cat = await strapi.services.category.find({ name: "Action" })
-
-    console.log(cat);
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
+    const { data: { products }} = await axios.get(gogApiUrl)
+    console.log(products[0]);
   }
 };
